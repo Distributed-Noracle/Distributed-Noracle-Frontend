42961d1a88f10cb75225ac5ca6192989284ed21e
bgoeschi <bgoeschi@yahoo.de>
2017-10-06 19:32:20 +0200

    changed port for steen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   redirectUrl: 'http://dbis.rwth-aachen.de/noracle/login',
-  hostUrl: 'http://steen.informatik.rwth-aachen.de:8080'
+  hostUrl: 'http://steen.informatik.rwth-aachen.de:9082'
 };
