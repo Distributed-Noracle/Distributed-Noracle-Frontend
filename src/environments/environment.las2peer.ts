73fa7ace42230889cc1300eaeeee6adabbde4b37
bgoeschi <bgoeschi@yahoo.de>
2017-10-10 15:09:10 +0200

    changed urls to local webconnector

--- src/environments/environment.las2peer.ts
+++ src/environments/environment.las2peer.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  redirectUrl: 'http://steen.informatik.rwth-aachen.de:9082/fileservice/v2.1.6/files/noracle/login',
-  hostUrl: 'http://steen.informatik.rwth-aachen.de:9082'
+  redirectUrl: 'http://localhost:8080/fileservice/v2.1.6/files/noracle/login',
+  hostUrl: 'http://localhost:8080'
 };
