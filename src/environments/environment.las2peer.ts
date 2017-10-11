290b00739ce6ebcec9bf583d859395632742c265
Peter de Lange <lange@dbis.rwth-aachen.de>
2017-10-11 08:56:57 +0100

    updated pathes for oidc redirect and base url

--- src/environments/environment.las2peer.ts
+++ src/environments/environment.las2peer.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  redirectUrl: 'http://localhost:9082/fileservice/v2.1.6/files/noracle/login',
-  hostUrl: 'http://localhost:8080'
+  redirectUrl: 'http://localhost:9082/fileservice/v2.2.0/files/noracle/login',
+  hostUrl: 'http://localhost:9082'
 };
