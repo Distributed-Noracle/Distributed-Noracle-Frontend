63dd333d7761cec99ad74bc044163aabeeb62577
Jan Benscheid <jan.benscheid@gmail.com>
2018-04-11 16:13:07 +0200

    support for multiple api hosts

---
+++ src/environments/environment.multiplex.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  redirectUrl: 'http://dbis.rwth-aachen.de/noracle/login',
+  hostUrl: ['http://las2peer.dbis.rwth-aachen.de:9070',
+  'http://las2peer.dbis.rwth-aachen.de:9071',
+  'http://las2peer.dbis.rwth-aachen.de:9072',
+  'http://las2peer.dbis.rwth-aachen.de:9073',
+  'http://las2peer.dbis.rwth-aachen.de:9074',
+  'http://las2peer.dbis.rwth-aachen.de:9075',
+  'http://las2peer.dbis.rwth-aachen.de:9076',
+  'http://las2peer.dbis.rwth-aachen.de:9077',
+  'http://las2peer.dbis.rwth-aachen.de:9078',
+  'http://las2peer.dbis.rwth-aachen.de:9079']
+};
