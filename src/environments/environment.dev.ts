74f59f09e89e9311f59ed63df74b57484d9cf2a2
Jan Benscheid <jan.benscheid@gmail.com>
2018-04-13 13:58:39 +0200

    update all environments to support multiple apis

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   redirectUrl: 'http://steen.informatik.rwth-aachen.de:9082/fileservice/v2.2.5/files/noracle/login',
-  hostUrl: 'http://steen.informatik.rwth-aachen.de:9082'
+  hostUrl: ['http://steen.informatik.rwth-aachen.de:9082']
 };
