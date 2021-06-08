e5eab30429e941845941da94f69ff5af32858058
Simon <simon.breuer@rwth-aachen.de>
2021-06-08 22:34:56 +0200

    Remove deprecated HttpModule + Add Dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {GraphViewModule} from './graph-view/graph-view.module';
 import {
@@ -31,7 +30,6 @@ import {OidcCookieStorage} from "./shared/oidc-cookie-storage/oidc-cookie-storag
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpModule,
     SharedModule,
     NavigationModule,
     LoginModule,
